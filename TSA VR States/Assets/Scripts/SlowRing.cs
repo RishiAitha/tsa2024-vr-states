111f7d6909dfb38433678357e9efcb787adcba5e
RishiAitha <110874460+RishiAitha@users.noreply.github.com>
2024-03-24 15:17:33 -0400

    added slow ring

---
+++ TSA VR States/Assets/Scripts/SlowRing.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowRing : MonoBehaviour
+{
+    private DetectRowing rowScript;
+
+    void Update()
+    {
+        rowScript = FindObjectOfType<DetectRowing>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            rowScript.currentVelocity *= 0.5f;
+        }
+    }
+
+}
